6984723236059d7dc711793c1354ebb2be4a4671
oussama-he <oussama.hel@gmail.com>
2020-06-13 10:58:09 +0100

    Modify Category and Bookmark models to use AutoSlugField

--- backend/api/bookmarks/models.py
+++ backend/api/bookmarks/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from django_extensions.db.fields import AutoSlugField
 
 
 class Category(MPTTModel):
     name = models.CharField(max_length=50, verbose_name='Category')
-    slug = models.SlugField()
+    slug = AutoSlugField(populate_from=["name"])
     description = models.TextField(blank=True, null=True)
     parent = TreeForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name="children")
 
@@ -21,6 +22,7 @@ class Category(MPTTModel):
 class Bookmark(models.Model):
     url = models.URLField(verbose_name="Bookmark URL", max_length=1000)
     title = models.CharField(max_length=250)
+    slug = AutoSlugField(populate_from=["title"])
     description = models.TextField(blank=True, null=True)
     category = TreeForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
     bookmarked_at = models.DateTimeField(auto_now_add=True, auto_now=False)
--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'mptt',
+    'django_extensions',
     'backend.api',
     'backend.api.bookmarks',
 ]
