d1c60aff1bfef99760d24b3bc734143ee0872620
oussama-he <oussama.hel@gmail.com>
2020-08-12 10:06:15 +0100

    Edit vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-// const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
     outputDir: 'dist',
     assetsDir: 'static',
-    // baseUrl: IS_PRODUCTION
-    // ? 'http://cdn123.com'
-    // : '/',
+    publicPath: IS_PRODUCTION
+    ? 'http://vue-django-bookmark.herokuapp.com/'
+    : '/',
     // For Production, replace set baseUrl to CDN
     // And set the CDN origin to `yourdomain.com/static`
     // Whitenoise will serve once to CDN which will then cache
